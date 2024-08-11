6f893759a846b7c20e7770299bcefb0a8f716744
Kyatastrophe <katharine.toth@gmail.com>
2024-08-11 17:44:56 -0400

    Added ability to read bars and print to channel

--- BakaBars/BakaBars.lua
+++ BakaBars/BakaBars.lua
@@ -1,3 +1,17 @@
-
-
 print("Welcome to BakaBars, " .. UnitName("player") .. "!")
+
+-- slash command definitions
+SLASH_BBREAD1 = "/bbread"
+SLASH_BBREAD2 = "/bakabarsread"
+SLASH_BBWRITE1 = "/bbwrite"
+SLASH_BBWRITE2 = "/bakabarswrite"
+
+-- read action bars
+local function readActionBar (msg, editBox)
+	for i = 1, 180 do
+		print(i, GetActionInfo(i))
+	end
+end
+
+-- slash commands to functions mapping
+SlashCmdList["BBREAD"] = readActionBar;
